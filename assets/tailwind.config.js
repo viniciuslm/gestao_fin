0cc97697a397b8649807ad4624b760264403993e
Vinicius Moreira <viniciuslm@gmail.com.br>
2022-06-28 17:19:10 -0300

    install tailwind

---
+++ assets/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./js/**/*.js', '../lib/*_web/**/*.*ex'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
--- config/dev.exs
+++ config/dev.exs
@@ -25,8 +25,15 @@ config :gestao_fin, GestaoFinWeb.Endpoint,
   debug_errors: true,
   secret_key_base: "5bzBS6HkGJnrrOJAGJFZPZ7zqfN76cZJOgArAi7Cni7EPTZ+c8v8xgXQ4TugmbvL",
   watchers: [
-    # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
