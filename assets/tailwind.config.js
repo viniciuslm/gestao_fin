123ffd41c7c02fba6fc60ed87989ee674bbaa948
Vinicius Moreira <viniciuslm@gmail.com.br>
2022-06-29 10:52:25 -0300

    instal tailwind

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./js/**/*.js', '../lib/*_web/**/*.*ex'],
+    content: ['./js/**/*.js', '../lib/*_web/**/*.*ex', './node_modules/flowbite/**/*.js'],
     theme: {
         extend: {},
     },
-    plugins: [],
+    plugins: [require('flowbite/plugin')],
 }
